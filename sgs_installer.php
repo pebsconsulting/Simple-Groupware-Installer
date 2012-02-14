77fee83e1e5f7a64fb35d2c311d3818ea61db44b
Thomas Bley <thbley+github@gmail.com>
2012-02-14 12:38:31 +0100

    increased version

--- sgs_installer.php
+++ sgs_installer.php
@@ -229,7 +229,7 @@ function out_header() {
 	form { margin: 0px; }
 	</style></head>
 	<body>
-	<div class='border headline'>Simple Groupware Installer v9</div>
+	<div class='border headline'>Simple Groupware Installer v10</div>
   ");
 }
 function out_footer() {
